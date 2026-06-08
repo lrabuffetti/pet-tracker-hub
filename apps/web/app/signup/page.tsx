4c23498627db71d350c69955d32f8fccc06ad0eb
Lucas Rabuffetti <lucas.rabuffetti@gmail.com>
2026-06-08 15:05:02 -0300

    feat: login and sign-up UI created for web

--- apps/web/app/signup/page.tsx
+++ apps/web/app/signup/page.tsx
@@ -2,73 +2,69 @@
 
 import { useState } from 'react'
 import { Button } from '@repo/ui'
+import { REGEX_EMAIL, REGEX_PASSWORD } from '@repo/ui/validation'
+import Link from 'next/link'
 
 const SignupPage = () => {
   const [isSubmitting, setIsSubmitting] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
 
-  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-  const regexPassword = /^.{6,}$/
-
-  const isFormValid = regexEmail.test(email) && regexPassword.test(password)
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value)
-  }
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value)
-  }
+  const isFormValid =
+    REGEX_EMAIL.test(email) &&
+    REGEX_PASSWORD.test(password) &&
+    confirmPassword === password
 
   const handleSignup = async () => {
     setIsSubmitting(true)
-    // Simulate an API call
     await new Promise((resolve) => setTimeout(resolve, 2000))
     setIsSubmitting(false)
     alert('Signup successful!')
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <h1 className="text-3xl font-bold mb-6">Sign Up</h1>
-      <form className="bg-white p-6 rounded shadow-md w-full max-w-sm border-2 border-solid border-gray-300">
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700 mb-2">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Enter your email"
-            value={email}
-            onChange={handleEmailChange}
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-gray-700 mb-2">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Enter your password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
+    <div className="flex min-h-screen flex-col items-center justify-center px-6">
+      <div className="flex w-[500px] flex-col gap-3 rounded-xl border border-gray-300 p-3">
+        <h1 className="mb-6 mt-6 text-center text-2xl font-bold">
+          Create an account
+        </h1>
+        <input
+          type="email"
+          className="mb-3 w-full rounded-xl border border-gray-300 p-3 focus:outline-none"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          className="mb-3 w-full rounded-xl border border-gray-300 p-3 focus:outline-none"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          className="mb-3 w-full rounded-xl border border-gray-300 p-3 focus:outline-none"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
         <Button
           type="button"
           onClick={handleSignup}
           disabled={isSubmitting || !isFormValid}
-          appName="web"
-          className="w-1/2 h-10 border-2 border-solid border-gray-300 rounded hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
+          className="w-full rounded-xl bg-indigo-600 py-3 text-lg font-bold text-white disabled:cursor-not-allowed disabled:opacity-50"
         >
           {isSubmitting ? 'Signing up...' : 'Sign Up'}
         </Button>
-      </form>
+        <p className="mt-6 text-center text-base">
+          Already have an account?{' '}
+          <Link href="/" className="text-[#2e78b7] underline">
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   )
 }
